6a07ab29d22619a55524804fe381eba562363ab2
Muhammad Ichsan <101854401+MhmmdIchsan@users.noreply.github.com>
2025-04-16 11:27:25 +0700

    feat: Update contactData.tsx

--- app/components/data/contactData.tsx
+++ app/components/data/contactData.tsx
@@ -4,25 +4,25 @@ export const contactData = [
   {
     icon: <FaEnvelope className="w-10 h-10 text-[#00FFB2] drop-shadow-glow" />,
     title: "Email",
-    text: "your-email@example.com",
-    href: "mailto:your-email@example.com",
+    text: "mhmmd.ichsan321@gmail.com",
+    href: "mailto:mhmmd.ichsan321@gmail.com",
   },
   {
     icon: <FaLinkedin className="w-10 h-10 text-[#00FFB2] drop-shadow-glow" />,
     title: "LinkedIn",
     text: "Connect with me",
-    href: "https://www.linkedin.com/in/your-linkedin",
+    href: "https://www.linkedin.com/in/mhmmdichsan",
   },
   {
     icon: <FaInstagram className="w-10 h-10 text-[#00FFB2] drop-shadow-glow" />,
     title: "Instagram",
     text: "Follow me",
-    href: "https://www.instagram.com/your-instagram",
+    href: "https://www.instagram.com/mhmd.ichsnn",
   },
   {
     icon: <FaGithub className="w-10 h-10 text-[#00FFB2] drop-shadow-glow" />,
     title: "GitHub",
     text: "Check out my projects",
-    href: "https://github.com/your-github",
+    href: "https://github.com/MhmmdIchsan",
   },
 ];
