c3cee0c234f82e537a98af03f4648ff41ee1aaf0
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2025-04-21 14:41:12 +0700

    refactor: remove unused import of Typewriter in Hero component

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { Typewriter } from "react-simple-typewriter";
+
 
 export default function Hero() {
   return (
