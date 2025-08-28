ccc59ac0d18a12027f2866feccc7ba28930c5378
Muhammad Ichsan <101854401+MhmmdIchsan@users.noreply.github.com>
2025-08-28 15:58:31 +0700

    Update contactData.tsx

--- app/components/data/contactData.tsx
+++ app/components/data/contactData.tsx
@@ -14,12 +14,6 @@ export const contactData = [
     href: "https://www.linkedin.com/in/mhmmdichsan",
   },
   {
-    icon: <FaInstagram className="w-10 h-10 text-[#00FFB2] drop-shadow-glow" />,
-    title: "Instagram",
-    text: "Follow me",
-    href: "https://www.instagram.com/mhmd.ichsnn",
-  },
-  {
     icon: <FaGithub className="w-10 h-10 text-[#00FFB2] drop-shadow-glow" />,
     title: "GitHub",
     text: "Check out my projects",
